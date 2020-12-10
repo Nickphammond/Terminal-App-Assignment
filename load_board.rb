326f62c6545d9fdfbd77f09dae75a03a1b935deb
Nicholas Hammond <hammondn649@gmail.com>
2020-12-11 10:01:36 +1100

    add timing functionality

--- load_board.rb
+++ load_board.rb
@@ -1,4 +1,6 @@
 
+require 'io/console'
+require "timeout"
 
 # Method to give default/empty board
 def board(x,y)
@@ -13,20 +15,47 @@ end
 
 
 #  Method to give character at position
-def grid_pos(x,y)
-    return board(x,y)
+def grid_pos(x,y, state)
+    if state == "a" && x==4 && y==7
+        return "a"
+    else
+        return board(x,y)
+    end
 end
 
 
 
 # Method to print board to terminal
 
-def print_board(numx,numy)
+def print_board(numx,numy,state)
     for i in 0..numx
         for j in 0..numy
-            print grid_pos(i,j)
+            print grid_pos(i,j,state)
         end
     print "\n"
     end
 end
-print_board(21,42)
+# print_board(21,42)
+
+
+def board_cycle()
+    system("clear")
+    print_board(21,42,"r")
+    begin
+        status = Timeout::timeout(2) {
+            
+            str = STDIN.getch
+            system("clear")
+            if str == "a"
+                print_board(21,42,str)
+            elsif str == 'q'
+                return "end"
+            end
+            
+        }
+    rescue
+        board_cycle
+    end
+end
+
+board_cycle
