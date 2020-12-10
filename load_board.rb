b199796e332ebefc836ef19e51b7ba3e9ce9ad5d
Nicholas Hammond <hammondn649@gmail.com>
2020-12-11 10:39:06 +1100

    add responsiveness to timed prompt

--- load_board.rb
+++ load_board.rb
@@ -2,6 +2,8 @@
 require 'io/console'
 require "timeout"
 
+state0 = 1
+
 # Method to give default/empty board
 def board(x,y)
     if y%14==0
@@ -14,10 +16,16 @@ def board(x,y)
 end
 
 
+def give_state(prev)
+    return (prev +1)%5
+end
+
 #  Method to give character at position
 def grid_pos(x,y, state)
-    if state == "a" && x==4 && y==7
+    if state == 0 && x==4 && y==7
         return "a"
+    elsif state == "b" && x==4 && y==7
+        return "b"
     else
         return board(x,y)
     end
@@ -38,24 +46,31 @@ end
 # print_board(21,42)
 
 
-def board_cycle()
+def board_cycle(state)
     system("clear")
-    print_board(21,42,"r")
-    begin
-        status = Timeout::timeout(2) {
-            
-            str = STDIN.getch
-            system("clear")
-            if str == "a"
-                print_board(21,42,str)
-            elsif str == 'q'
-                return "end"
-            end
-            
-        }
-    rescue
-        board_cycle
+    puts state
+    if state != 100
+        print_board(21,42,state)
+        begin
+            status = Timeout::timeout(2) {
+                
+                str = STDIN.getch
+                system("clear")
+                if str == "a" && state == 0
+                    return board_cycle(100)
+                elsif str == 'q'
+                    return "end"
+                end
+                
+                return nick.to_i
+            }
+        rescue
+            state = give_state(state)
+            board_cycle(state)
+        end
+    else
+        puts "You Win!!"
     end
 end
 
-board_cycle
+board_cycle(state0)
